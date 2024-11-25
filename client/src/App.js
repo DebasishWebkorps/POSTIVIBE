0c01605ae27ef255066c5c9d6e4424023a1f2536
DebasishWebKorps <debasish@webkorps.com>
2024-11-26 00:04:14 +0530

    correction app name

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to POSTVIBE</h1>
+      <h1>Welcome to POSTIVIBE</h1>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 
+app.use('/', (req, res, next) => {
+    res.send('<h1>Welcome to POSTIVIBE server</h1>')
+})
+
 app.listen(3000, () => {
     console.log('server started')
 })
